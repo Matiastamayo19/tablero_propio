d919a50d25e5420457906a5a67146b85b5d27e96
Matiastamayo19 <mtamayog3@eafit.edu.co>
2026-04-16 10:17:12 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,8 +7,8 @@ with st.sidebar:
     st.subheader("Propiedades del tablero")
 
     st.subheader("Dimensiones del tablero")
-    canvas.width = st.slider("Ancho del tablero", 300, 700, 500, 50)
-    canvas.height: st.slider("Alto del tablero", 200, 600, 300, 50)
+    canvas_width = st.slider("Ancho del tablero", 300, 700, 500, 50)
+    canvas_height: st.slider("Alto del tablero", 200, 600, 300, 50)
 
     drawing_mode = st.selectbox(
         "Herramienta de dibujo:",
